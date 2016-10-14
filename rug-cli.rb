fc83731acfe0a12106d5f703a856789b7593def7
Christian Dupuis <christian@dupuis.is>
2016-10-14 08:08:41 -0700

    Update rug-cli to 0.0.1-20161014150246

--- rug-cli.rb
+++ rug-cli.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RugCli < Formula
   homepage 'http://github.com/atomist/rug-cli/'
-  url 'https://github.com/atomist/homebrew-tap/releases/download/0.0.1-20161014023810/rug-cli-0.0.1-20161014023810-bin.tar.gz'
-  version '0.0.1-20161014023810'
-  sha256 'f0ef2a6f46dd3712d1e75f44e01eb16273f1e7ca8a276ed8c0f1e7199de8f0be'
+  url 'https://github.com/atomist/homebrew-tap/releases/download/0.0.1-20161014150246/rug-cli-0.0.1-20161014150246-bin.tar.gz'
+  version '0.0.1-20161014150246'
+  sha256 '74a797332592798f648a68d1f0824a6a269eceaf624e83f2d75bd46bfe4a133c'
 
   def install
     root = '.'
