8108f11187e2b8db56d3ecdcb67dd9f2c448ad71
Christian Dupuis <christian@dupuis.is>
2016-10-18 12:45:37 -0700

    Just a test

--- Formula/rug-cli.rb
+++ Formula/rug-cli.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class RugCli < Formula
   homepage 'http://github.com/atomist/rug-cli/'
-  url 'https://github.com/atomist/homebrew-tap/releases/download/0.0.1-20161014150246/rug-cli-0.0.1-20161014150246-bin.tar.gz'
+  url 'https://github.com/atomist/homebrew-tap/releases/download/0.5.1-20161014150246/rug-cli-0.0.1-20161014150246-bin.tar.gz'
   version '0.0.1-20161014150246'
   sha256 '74a797332592798f648a68d1f0824a6a269eceaf624e83f2d75bd46bfe4a133c'
 
