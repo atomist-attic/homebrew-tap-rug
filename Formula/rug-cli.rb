8f5ea70cd6040297ad8b8868fbabc6c84905ef59
Travis CI <travis-ci@atomist.com>
2017-01-04 20:03:29 +0000

    Update formula for rug-cli

--- Formula/rug-cli.rb
+++ Formula/rug-cli.rb
@@ -1,8 +1,8 @@
 class RugCli < Formula
   desc "CLI for creating and running Rugs"
   homepage "https://github.com/atomist/rug-cli"
-  url "https://atomist.jfrog.io/atomist/libs-release/com/atomist/rug-cli/0.18.1/rug-cli-0.18.1-bin.tar.gz"
-  sha256 "a489999ffef87606178f21d704276d5f793d4d43c54202fae8879b9c692bc37a"
+  url "https://atomist.jfrog.io/atomist/libs-release/com/atomist/rug-cli/0.19.0/rug-cli-0.19.0-bin.tar.gz"
+  sha256 "93c15277ad4e14afb075e852963e6027a2d1f2490e6d39daca61f8969935e493"
   head "https://github.com/atomist/rug-cli.git"
 
   bottle :unneeded
